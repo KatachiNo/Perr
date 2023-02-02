8893b663e8f778b1d6269fc3dc939e751b4b451d
koeno <t-7892009@mail.ru>
2023-02-02 23:32:27 +0300

    начало работы над авторизацией и реализации api

--- cmd/perrApp/main.go
+++ cmd/perrApp/main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 
 	pdb "github.com/KatachiNo/Perr/internal/postgresDataBase"
 	"github.com/gorilla/mux"
@@ -42,10 +45,34 @@ func main() {
 
 	fmt.Print("Are u ready?\n")
 
-	//serv := new(transport.Server)
+	var vspec = viper.New()
+	vspec.AddConfigPath(".")
+	vspec.SetConfigName("PerrAppE")
+	vspec.SetConfigFile("env")
+	//vspec.AutomaticEnv()
+	e := vspec.ReadInConfig()
+
+	if e != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", e))
+	}
+
+	t := vspec.GetInt("DB_PASSWORD")
+	fmt.Printf("%s sd", t)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/products/all", getAllProducts)
+
+	r.HandleFunc("/login/authorization", authorization)
+	r.HandleFunc("/login/registration", registration)
+	r.HandleFunc("/login/changeData", changeData)
+	r.HandleFunc("/login/forgetPassword", forgetPassword)
+
+	r.HandleFunc("/products/all", getAllProducts).Methods("GET")
+	r.Handle("/products/add", isAuthorized(addProduct)).Methods("POST")
+	r.HandleFunc("/products/changeProductItem", changeProduct)
+	r.HandleFunc("/products/delete", deleteProduct)
+
+	r.HandleFunc("/products/getPriceStory", deleteProduct)
+	r.HandleFunc("/products/cha", deleteProduct)
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 
@@ -53,6 +80,107 @@ func main() {
 	// 	log.Fatal(http.ListenAndServe(":8000", r))
 	// }
 
+}
+
+func changeData(w http.ResponseWriter, r *http.Request) {
+
+}
+func forgetPassword(w http.ResponseWriter, r *http.Request) {
+
+}
+func addProduct(w http.ResponseWriter, r *http.Request) {
+
+}
+func changeProduct(w http.ResponseWriter, r *http.Request) {
+
+}
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+
+}
+func registration(w http.ResponseWriter, r *http.Request) {
+
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var userTest = User{
+	Username: "1",
+	Password: "1",
+}
+
+func authorization(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	var u User
+	json.NewDecoder(r.Body).Decode(&u)
+
+}
+
+func checkAuthorization(u User) string {
+	if userTest.Username != u.Password && userTest.Password != u.Password {
+		fmt.Print("is not ok")
+	}
+
+	validToken, err := generationOfTokenJWT()
+
+	if err == nil {
+		fmt.Print(err)
+	}
+	return validToken
+
+}
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Connection", "close")
+		defer r.Body.Close()
+
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				w.WriteHeader(http.StatusForbidden)
+				w.Header().Add("Content-Type", "application/json")
+				return
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+
+		} else {
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
+}
+
+var mySigningKey = []byte("johenews")
+
+func generationOfTokenJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "Elliot Forbes"
+	claims["exp"] = time.Now().Add(time.Hour * 2160).Unix()
+
+	tokenString, err := token.SignedString(mySigningKey)
+
+	if err != nil {
+		fmt.Errorf("Something went wrong: %s", err.Error())
+	}
+
+	return tokenString, nil
 }
 
 func getAllProducts(w http.ResponseWriter, r *http.Request) {
@@ -66,35 +194,28 @@ func getAllProducts(w http.ResponseWriter, r *http.Request) {
 func ConnectionWithDB(resp string) []ProductsTable {
 	var Data []ProductsTable
 
-	// viper.AddConfigPath("/configs")
-	// viper.SetConfigName("config")
-	// var connStr pdb.ConfigDB = pdb.ConfigDB{
-	// 	Host:     viper.GetString("db.host"),
-	// 	Port:     viper.GetString("db.port"),
-	// 	Username: viper.GetString("db.username"),
-	// 	Password: os.Getenv("DB_PASSWORD"),
-	// 	DBName:   viper.GetString("db.dbname"),
-	// 	SSLMode:  viper.GetString("db.host"),
-	// }
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./configs/")
+	err := viper.ReadInConfig()
+
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
 
 	var connStr pdb.ConfigDB = pdb.ConfigDB{
-		Host:     "localhost",
-		Port:     "5432",
-		Username: "postgres",
-		Password: "1234",
-		DBName:   "postgres",
-		SSLMode:  "disable",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
 	}
 
 	db, err333 := pdb.ConnectToDB(connStr)
 
 	if err333 != nil {
-		log.Fatalf("error loading env variables: %s", err333.Error())
+		log.Fatalf(": %s", err333.Error())
 	}
-
-	//if err := godotenv.Load(); err != nil {
-	//	log.Fatalf("error loading env variables: %s", err.Error())
-	//}
 
 	rows, _ := db.Query(`SELECT * FROM "Products"`)
 	for rows.Next() {
--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/KatachiNo/Perr
 go 1.19
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/lib/pq v1.10.7 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
