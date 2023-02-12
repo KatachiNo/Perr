4524942a971283f59e6b95122f8db3ce592fc521
koeno <t-7892009@mail.ru>
2023-02-13 02:36:30 +0300

    changes1

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,10 +1,10 @@
 package user
 
 import (
+	"github.com/KatachiNo/Perr/internal/handlers"
 	"io"
 	"net/http"
 
-	"github.com/KatachiNo/Perr/internal/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -12,7 +12,7 @@ const (
 	loginAuthorization  = "/login/authorization"
 	loginRegistration   = "/login/registration"
 	loginChangeData     = "/login/changeData"
-	loginforgetPassword = "/login/forgetPassword"
+	loginForgetPassword = "/login/forgetPassword"
 
 	productsAll               = "/products/all"
 	productsAdd               = "/products/add"
@@ -23,17 +23,32 @@ const (
 	testHey = "/test"
 )
 
+func New() handlers.Handler {
+	return &handler{}
+}
+
 type handler struct {
 }
 
 func (h *handler) Register(router *mux.Router) {
 	router.HandleFunc(testHey, hey).Methods("GET")
+
+	router.HandleFunc(productsAll, getProductsAll).Methods("GET")
+	router.HandleFunc(productsAdd, prodAdd).Methods("POST")
+	router.HandleFunc(productsChangeProductItem, hey).Methods("PATCH")
+	router.HandleFunc(productsDeleteItem, hey).Methods("DELETE")
+	router.HandleFunc(productsPriceStory, hey).Methods("GET")
+
+}
+
+func getProductsAll(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func prodAdd(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func hey(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hey")
-}
-
-func New() handlers.Handler {
-	return &handler{}
 }
